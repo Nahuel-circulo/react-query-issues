357483febe84679392a2f9d28b436cb0fe771946
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-05-16 18:01:14 -0300

    instalacion de axios  y creacion de interface lable

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+import { githubApi } from '../../api/githubApi';
+import { Label } from '../../interfaces/label';
+
+
+const getLabels = async ():Promise<Label[]> => {
+  const { data } = await githubApi.get<Label[]>('/labels');
+  return data
+}
+
 
 export const LabelPicker = () => {
+
+  const labelsQuery = useQuery(['labels'], getLabels)
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
-        >
-            Primary
-        </span>
-        
+      <span
+        className="badge rounded-pill m-1 label-picker"
+        style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+      >
+        Primary
+      </span>
+
     </div>
   )
 }
