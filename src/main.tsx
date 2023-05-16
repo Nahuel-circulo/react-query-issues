18b722289e572f78861cdd66273f5ee23d5a7c48
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2023-05-16 16:55:05 -0300

    instalacion react query y react query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,17 @@ import { router } from './router';
 //! Remover enable css source maps
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <QueryClientProvider client={ queryClient }>
+      <ReactQueryDevtools/>
     <RouterProvider router={ router } />
+    </QueryClientProvider>
   </React.StrictMode>
 )
